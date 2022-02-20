f6114264532fd398368479387d82cd1a79fbdece
0x0000ff <0x0000ff@github.com>
2022-02-20 19:38:50 +0000

    test

---
+++ tooltips.py
@@ -0,0 +1,18 @@
+SPEED, SENSITIVITY, INERTIA, REACH, DRAGHYS, MINDRAG, THRESH, UPTHRESH, \
+    ZTIME, JENKS, PRESS_TO_SELECT, SKIPBACK, EXT_DEV = \
+    "Sensitivity.", \
+    "Cursor speed.", \
+    "Described as \"negative inertia.\" It acts more like friction. High" \
+        "values cause the cursor to snap backward when the Trackpoint" \
+            "is released.", \
+    "Backup for Z-axis press.",\
+    "Drag hysteresis (how hard it is to drag with Z-axis pressed).", \
+    "Minimum amount of force needed to trigger dragging.", \
+    "Minimum value for a Z-axis press.", \
+    "Used to generate a 'click' on Z-axis.", \
+    "How sharp of a press ", \
+    "Minimum curvature for double click.", \
+    "Press to select ", \
+    "Supress movement after drag release.", \
+    "Disable external device."  
+
