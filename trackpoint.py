4e630cef3f03b0936fb68a5f9a8fe6088aed0460
0x0000ff <0x0000ff@github.com>
2022-02-21 03:49:49 +0000

    test

--- trackpoint.py
+++ trackpoint.py
@@ -1,16 +1,20 @@
-trackpointpath="/sys/devices/platform/i8042/serio1/driver/serio2/"
-tf=trackpointpath + "speed"
+tp_labels = ["sensitivity", "speed", "inertia", "reach", "draghys", \
+    "mindrag", "thresh", "upthresh", "ztime", "jenks", "press_to_select", \
+        "skipback", "ext_dev"]
+tp_path="/sys/devices/platform/i8042/serio1/driver/serio2/"
 
 def get_setting(tf):
-    f = open(tf, "r")
-    r = f.read()
+    f = open(tf, "rt",newline='\n')
+    r = f.readline()
     f.close()
-    return r
+    return r.replace('\n', '')
+    #return r
 
 def set_setting(tf, ts):
     f = open(tf, "r+")
     f.write(ts)
     f.close()
 
-set_setting(trackpointpath + "speed", "30")
-print("speed = ", get_setting(trackpointpath + "speed"))
+#set_setting(tp_path + "speed", "30")
+print (tp_path + tp_labels[1])
+print(get_setting(tp_path + tp_labels[1]))
--- trackpoint_gtk.py
+++ trackpoint_gtk.py
@@ -7,12 +7,53 @@ class MyWindow(Gtk.Window):
     def __init__(self) -> None:
         super().__init__(title="Trackpoint Settings")
 
-        self.scale1 = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.button = Gtk.Button(label="apply")
-        self.button.connect("clicked", self.on_button_clicked)
-        #self.add(self.button)
-        self.add(self.scale1)
-    
+        self.box1 = Gtk.Box(spacing=6)
+        self.add(self.box1)
+
+        self.box2 = Gtk.VBox(spacing=6)
+        self.box1.pack_start(self.box2, True, True, 0)
+        self.box3 = Gtk.VBox(spacing=6)
+        self.box1.pack_start(self.box3, True, True, 0)
+        
+        self.SensitivityScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.SensitivityScale, True, True, 0)   
+        
+        self.SpeedScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.SpeedScale, True, True, 0)   
+        
+        self.InertiaScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.InertiaScale, True, True, 0)   
+        
+        self.ReachScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.ReachScale, True, True, 0)   
+        
+        self.DraghysScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.DraghysScale, True, True, 0)   
+        
+        self.MindragScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.MindragScale, True, True, 0)   
+        
+        self.ThreshScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box2.pack_start(self.ThreshScale, True, True, 0)   
+        
+        self.UpthreshScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.UpthreshScale, True, True, 0)   
+        
+        self.ZtimeScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.ZtimeScale, True, True, 0)   
+        
+        self.JenksScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.JenksScale, True, True, 0)   
+        
+        self.Press_to_selectScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.Press_to_selectScale, True, True, 0)   
+        
+        self.SkipbackScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.SkipbackScale, True, True, 0)   
+        
+        self.Ext_devScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
+        self.box3.pack_start(self.Ext_devScale, True, True, 0)   
+        
     def on_button_clicked(self,widget):
         print("Hello World")
 
