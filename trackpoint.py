92cdafba3ef194aab029cf52b66bb47997b96b3e
0x0000ff <0x0000ff@github.com>
2022-02-27 04:52:05 +0000

    added more handling of config file

--- trackpoint.py
+++ trackpoint.py
@@ -1,7 +1,10 @@
+import configparser
+Config=configparser.ConfigParser()
+
+tp_path="/sys/devices/platform/i8042/serio1/driver/serio2/"
 tp_labels = ["sensitivity", "speed", "inertia", "reach", "draghys", \
     "mindrag", "thresh", "upthresh", "ztime", "jenks", "press_to_select", \
         "skipback", "ext_dev"]
-tp_path="/sys/devices/platform/i8042/serio1/driver/serio2/"
 
 def get_setting(tf):
     f = open(tf, "rt",newline='\n')
@@ -15,6 +18,22 @@ def set_setting(tf, ts):
     f.write(ts)
     f.close()
 
-#set_setting(tp_path + "speed", "30")
-print (tp_path + tp_labels[1])
-print(get_setting(tp_path + tp_labels[1]))
+def store_default_settings():
+    Config.read('trackpoint.conf')
+    for i in range(12):
+        if (Config.has_option('DEFAULT', tp_labels[i]) == False \
+                or Config ['DEFAULT'] [tp_labels[i]] == ''):
+            Config['DEFAULT'] [tp_labels[i]] = get_setting(tp_path + tp_labels[i])
+        else: 
+            print ("BEANS")
+    with open('trackpoint.conf', 'w') as configfile:
+        Config.write(configfile)
+
+def retrieve_config_settings():
+    Config.read('trackpoint.conf')
+    for i in range(12):
+        if (Config.has_option('DEFAULT', tp_labels[i]) == True \
+                or Config ["DEFAULT"] [tp_labels[i]] != ''):
+           print(Config ['DEFAULT'] [tp_labels[i]])      
+store_default_settings() 
+retrieve_config_settings()
