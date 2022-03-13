347d5ec247a667ba68dfdd14f7519f75d7867e08
0x0000ff <0x0000ff@github.com>
2022-03-13 05:08:20 +0000

    something

--- trackpoint.py
+++ trackpoint.py
@@ -20,20 +20,19 @@ def set_setting(tf, ts):
 
 def store_default_settings():
     Config.read('trackpoint.conf')
-    for i in range(12):
-        if (Config.has_option('DEFAULT', tp_labels[i]) == False \
-                or Config ['DEFAULT'] [tp_labels[i]] == ''):
-            Config['DEFAULT'] [tp_labels[i]] = get_setting(tp_path + tp_labels[i])
-        else: 
-            print ("BEANS")
+    for i in tp_labels:
+        if (Config.has_option('DEFAULT', i) == False \
+                or Config ['DEFAULT'] [i] == ''):
+            Config['DEFAULT'] [i] = get_setting(tp_path + i)
     with open('trackpoint.conf', 'w') as configfile:
         Config.write(configfile)
 
 def retrieve_config_settings():
     Config.read('trackpoint.conf')
-    for i in range(12):
-        if (Config.has_option('DEFAULT', tp_labels[i]) == True \
-                or Config ["DEFAULT"] [tp_labels[i]] != ''):
-           print(Config ['DEFAULT'] [tp_labels[i]])      
+    tp_values = []
+    for i in tp_labels:
+        tp_values.append(Config ['DEFAULT'] [i])     
+    return (tp_values)
+
 store_default_settings() 
-retrieve_config_settings()
+#print (retrieve_config_settings())
--- trackpoint_gtk.py
+++ trackpoint_gtk.py
@@ -1,4 +1,5 @@
 import gi
+import trackpoint
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -6,56 +7,97 @@ from gi.repository import Gtk
 class MyWindow(Gtk.Window):
     def __init__(self) -> None:
         super().__init__(title="Trackpoint Settings")
+        self.Box1 = Gtk.VBox(spacing=6)
+        self.add(self.Box1)
+        self.Box2 = Gtk.Box(spacing=6)
+        self.Box1.pack_start(self.Box2, True, True, 0)
 
-        self.box1 = Gtk.Box(spacing=6)
-        self.add(self.box1)
+        self.Box3 = Gtk.VBox(spacing=6)
+        self.Box2.pack_start(self.Box3, True, True, 0)
+        self.Box4 = Gtk.VBox(spacing=6)
+        self.Box2.pack_start(self.Box4, True, True, 0)
+        self.ButtonBox = Gtk.Box(spacing=6)
+        self.Box1.pack_start(self.ButtonBox, True, True, 0)
 
-        self.box2 = Gtk.VBox(spacing=6)
-        self.box1.pack_start(self.box2, True, True, 0)
-        self.box3 = Gtk.VBox(spacing=6)
-        self.box1.pack_start(self.box3, True, True, 0)
+        self.tp_values = trackpoint.retrieve_config_settings()
         
         self.SensitivityScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.SensitivityScale, True, True, 0)   
+        self.SensitivityScale.set_value(int(self.tp_values[0]))
+        self.Box3.pack_start(self.SensitivityScale, True, True, 0)   
         
         self.SpeedScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.SpeedScale, True, True, 0)   
+        self.SpeedScale.set_value(int(self.tp_values[1]))
+        self.Box3.pack_start(self.SpeedScale, True, True, 0)   
         
         self.InertiaScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.InertiaScale, True, True, 0)   
+        self.InertiaScale.set_value(int(self.tp_values[2]))
+        self.Box3.pack_start(self.InertiaScale, True, True, 0)   
         
         self.ReachScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.ReachScale, True, True, 0)   
+        self.ReachScale.set_value(int(self.tp_values[3]))
+        self.Box3.pack_start(self.ReachScale, True, True, 0)   
         
-        self.DraghysScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.DraghysScale, True, True, 0)   
+        self.DraghysScale = Gtk.Scale.new_with_range(0, 0, 255, 1)
+        self.DraghysScale.set_value(int(self.tp_values[4]))
+        self.Box3.pack_start(self.DraghysScale, True, True, 0)   
         
         self.MindragScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.MindragScale, True, True, 0)   
+        self.MindragScale.set_value(int(self.tp_values[5]))
+        self.Box3.pack_start(self.MindragScale, True, True, 0)   
         
         self.ThreshScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box2.pack_start(self.ThreshScale, True, True, 0)   
+        self.ThreshScale.set_value(int(self.tp_values[6]))
+        self.Box3.pack_start(self.ThreshScale, True, True, 0)   
         
-        self.UpthreshScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.UpthreshScale, True, True, 0)   
+        self.UpthreshScale = Gtk.Scale.new_with_range(0, 0, 255, 1)
+        self.UpthreshScale.set_value(int(self.tp_values[7]))
+        self.Box4.pack_start(self.UpthreshScale, True, True, 0)   
         
         self.ZtimeScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.ZtimeScale, True, True, 0)   
+        self.ZtimeScale.set_value(int(self.tp_values[8]))
+        self.Box4.pack_start(self.ZtimeScale, True, True, 0)   
         
         self.JenksScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.JenksScale, True, True, 0)   
+        self.JenksScale.set_value(int(self.tp_values[9]))
+        self.Box4.pack_start(self.JenksScale, True, True, 0)   
         
         self.Press_to_selectScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.Press_to_selectScale, True, True, 0)   
+        self.Press_to_selectScale.set_value(int(self.tp_values[10]))
+        self.Box4.pack_start(self.Press_to_selectScale, True, True, 0)   
         
         self.SkipbackScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.SkipbackScale, True, True, 0)   
+        self.SkipbackScale.set_value(int(self.tp_values[11]))
+        self.Box4.pack_start(self.SkipbackScale, True, True, 0)   
         
         self.Ext_devScale = Gtk.Scale.new_with_range(0, 0, 200, 1)
-        self.box3.pack_start(self.Ext_devScale, True, True, 0)   
+        self.Ext_devScale.set_value(int(self.tp_values[12]))
+        self.Box4.pack_start(self.Ext_devScale, True, True, 0)   
         
+        self.ListofScales = (self.Box3.get_children())
+        self.ListofScales = self.ListofScales + self.Box4.get_children()
+
+        self.DefaultButton = Gtk.Button(label="Default")
+        self.DefaultButton.connect("clicked", self.on_button_clicked)
+        self.ButtonBox.pack_start(self.DefaultButton, True, True, 0)
+
+        self.ResetButton = Gtk.Button(label="Reset")
+        self.ResetButton.connect("clicked", self.on_button_clicked)
+        self.ButtonBox.pack_start(self.ResetButton, True, True, 0)
+
+        self.ApplyButton = Gtk.Button(label="Apply")
+        self.ApplyButton.connect("clicked", self.on_button_clicked)
+        self.ButtonBox.pack_start(self.ApplyButton, True, True, 0)
+
+
     def on_button_clicked(self,widget):
         print("Hello World")
+        for i in self.ListofScales:
+            print (i.get_value())
+        print (self.tp_values)
+    
+    def on_scale_draw(self,widget,something):
+            widget.set_value(200)
+
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
